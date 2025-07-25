f5726dd0499313bbe421e415d22e94ce9e83f54f
AkashCreates <akashmaityofficial@gmail.com>
2025-07-25 20:04:30 +0530

    nine

---
+++ JavaScript Fundamentals/8.Operators-maths.js
@@ -0,0 +1,71 @@
+// let num = 121 ** (1 / 2); //square root using exponentiation
+// console.log(num);
+
+// num = 9 ** 2;
+// console.log(num);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+// console.log(typeof (1 + 2 + "2"));
+
+// do the binary + operator id only, that support stings as a operation
+
+// console.log("6" / 2);
+// console.log("6" / "2");
+// console.log("6" * "2");
+// console.log("6" - "2");
+// let x = 6 - "2";
+// console.log(x);
+// console.log(typeof x);
+// let y = "6" - "2";
+// console.log(y)
+// console.log(typeof x);
+
+// let z = "6" + "2"; //only + can work on strings
+// console.log(z)
+// console.log(typeof z);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+" ");
+// let x = "3",
+//   y = "4";
+// let sum = x + y;
+// console.log(sum);
+// sum = +x + +y;
+// console.log(sum);
+// sum = Number(x) + Number(y);
+// console.log(sum);
+
+// let counter = 1;
+// let a = counter++;
+
+// console.log(a);
+// console.log(a);
+
+// let a = 1,
+//   b = 1;
+
+// let c = ++a;
+// let d = b++;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);
+// console.log(x);
+
+
+// let x = " \t " - 2;
+// console.log(x);
+
+// let a = +prompt("First number?", 1);
+// let b = +prompt("Second number?", 2);
+
+// alert(a + b);
---
+++ JavaScript Fundamentals/9.Comparisons.js
@@ -0,0 +1,29 @@
+// let check = "A" < "a";
+// console.log(check);
+// let check1 = "abd" < "abdd";
+// console.log(check1);
+
+// let x = "2";
+// let y = 2;
+// let check1 = x === y;
+// console.log(check1);
+
+// let x;
+// console.log(x);
+// x = Number(x);
+// console.log(x);
+
+// let x = null;
+// console.log(x);
+// x = Number(x);
+// console.log(x);
+
+// let x = null == undefined;
+// console.log(x);
+
+// console.log( null > 0 );  // (1) false
+// console.log( null == 0 ); // (2) false
+// console.log( null >= 0 ); // (3) true
+
+
+ 
