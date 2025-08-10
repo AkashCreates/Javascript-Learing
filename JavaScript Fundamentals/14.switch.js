ecb1533adca1cac95f7e0196ae7875d3c3ddded8
AkashCreates <akashmaityofficial@gmail.com>
2025-08-10 12:06:43 +0530

    switch

---
+++ JavaScript Fundamentals/14.switch.js
@@ -0,0 +1,103 @@
+// let day;
+// day = 3;
+
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saterday");
+//     break;
+//   case 7:
+//     console.log("sunday");
+//     break;
+
+//   default:
+//     console.log("n00o");
+// }
+
+// Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nihil harum nobis non molestias ea, eos molestiae ducimus rerum dolorem fuga consectetur error quibusdam ipsum asperiores nulla, dolores debitis sed.
+
+// let a = 3;
+
+// switch (a) {
+//   case 4:
+//     console.log("Right!");
+//     break;
+
+//   case 3:
+//     break;
+//   case 5:
+//     console.log("Wrong!");
+//     console.log("Why don't you take a math class?");
+//     break;
+
+//   default:
+//     console.log("The result is strange. Really.");
+// }
+
+// Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum magnam dolore numquam itaque quos architecto in quo laudantium commodi porro, ullam ut vel voluptates eius quae minima nihil maxime nobis.
+
+// tasks
+
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+// --------------------------------
+// let browser = "Chrome";
+
+// if (browser === "Edge") {
+//   console.log("You've got the Edge!");
+// } else if (
+//   browser === "Chrome" ||
+//   browser === "Firefox" ||
+//   browser === "Safari" ||
+//   browser === "Opera"
+// ) {
+//   console.log("Okay we support these browsers too");
+// } else {
+//   console.log("We hope that this page looks ok!");
+// }
+
+// Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam possimus iure aliquid rem laborum neque eos dolore esse nulla nostrum! Iste pariatur vero expedita rem quaerat at quas laborum magni.
+
+// let a = 2;
+
+// switch (a) {
+//   case 1:
+//     console.log("one");
+//     break;
+//   case 2:
+//     console.log("It's Two");
+//     break;
+//   case 3:
+//   case 4:
+//     console.log("its 3 or 4");
+//     break;
+//   default:
+//     console.log("Bewond my understading");
+// }
