297a37029af9afb29444c3dcf3c37a8680f95dc6
AkashCreates <akashmaityofficial@gmail.com>
2025-08-17 21:12:07 +0530

    function

---
+++ JavaScript Fundamentals/15.functions.js
@@ -0,0 +1,45 @@
+// function
+
+// let x = 13;
+
+// {
+//   let x = 10;
+//   console.log(x);
+// }
+// console.log(x);
+
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iusto est a repudiandae voluptas quis aut quae cum. Quasi consequatur asperiores, tenetur non voluptatem vitae explicabo doloremque aspernatur temporibus aliquam.
+
+// function showMessage(from, text = "no text given") {
+//   from = "*" + from + "*"; // make "from" look nicer
+
+//   console.log(from + ": " + text);
+// }
+
+// let from = "Ann";
+
+// showMessage(from, "Hello"); // *Ann*: Hello
+
+// // the value of "from" is the same, the function modified a local copy
+// console.log(from); // Ann
+
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laudantium quaerat pariatur repudiandae sunt eius voluptas dolor dolores quasi, nobis quis assumenda distinctio ipsam tempore sequi vero laborum eum voluptate.
+
+// function empty() {
+//   return "no text given";
+// }
+// function showMessage(from, text = empty()) {
+//   from = "*" + from + "*";
+
+//   console.log(from + ": " + text);
+// }
+
+// let from = "Ann";
+
+// showMessage(from);
+
+// console.log(from);
+
+// Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, laborum praesentium? Odio aut illum fuga molestiae beatae recusandae facere? Nostrum asperiores adipisci sint vero doloribus ex unde enim quaerat facere!
+
+// console.log(null || 0 || undefined);
